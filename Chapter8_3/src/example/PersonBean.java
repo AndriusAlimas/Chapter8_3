9cdb750fd45d5f4379a51a32d9fb27896b502e8d
Andrius <andriusjavait@gmail.com>
2017-02-02 22:21:26 +0000

    created PersonBean code that treated as Java Bean

---
+++ Chapter8_3/src/example/PersonBean.java
@@ -0,0 +1,33 @@
+package example;
+
+import java.io.Serializable;
+
+public class PersonBean implements Serializable{
+		// FIELDS:
+	 	private String firstName;
+	    private String lastName;
+	    // NO-ARG CONSTRUCTOR(need for bean)
+	    public PersonBean() {
+	    }
+	    // overloaded CONSTRUCTOR:
+	    public PersonBean(String string, String string2) {
+	        this.firstName = string;
+	        this.lastName = string2;
+	    }
+	    // SETTERS:
+	    public void setFirstName(String string) {
+	        this.firstName = string;
+	    }
+
+	    public void setLastName(String string) {
+	        this.lastName = string;
+	    }
+	    // GETTERS: 
+	    public String getFirstName() {
+	        return this.firstName;
+	    }
+
+	    public String getLastName() {
+	        return this.lastName;
+	    }
+}
